b614e63c3bdd9edbbbd33a4721aa1107385ac071
Teo_You <teoyou881@gmail.com>
2025-05-30 00:04:54 -0400

    edit product-detail to public

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Login from './page/Login.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {useState} from 'react';
 import PrivateRoute from './route/PrivateRoute.jsx';
+import Search from './page/Search.jsx';
+import ProductDetail from './page/ProductDetail.jsx';
 
 function App() {
 
@@ -17,7 +19,11 @@ function App() {
         <Routes>
           <Route path="/hc_h_m/" element={<ProductAll/>}></Route>
           <Route path="/hc_h_m/login" element={<Login setAuth={setAuth} auth={auth}/>}></Route>
-          <Route path="/hc_h_m/product/:id" element={<PrivateRoute auth={auth}/>}></Route>
+          // todo
+          //search page 구현
+          <Route path="/hc_h_m/search" element={<Search/>}></Route>
+          {/*<Route path="/hc_h_m/product/:id" element={<PrivateRoute auth={auth}/>}></Route>*/}
+          <Route path="/hc_h_m/product/:id" element={<ProductDetail/>}></Route>
         </Routes>
 
       </>
